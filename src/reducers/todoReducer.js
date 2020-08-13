8dd4ce568abc90cd1eed5d2a4087e0f2254d01cc
rosariov21 <rosariovelazquez47@yahoo.com>
2020-08-13 09:49:44 -0400

    second Commit

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,18 +1,18 @@
-import  * as actions from  '../actions/todoActions'
+// import  * as actions from  '../actions/todoActions'
 
-export const initialState ={
-    // posts:[],
-    // loading: false,
-    // hasErrors:false
-}
+// export const initialState ={
+//     // posts:[],
+//     // loading: false,
+//     // hasErrors:false
+// }
 
-export default function todoReducer(state=initialState, action){
-    switch(action.type){
-        case actions.UPDATE_TODO_LIST:
-            return {savedTodoItems: [...initialState.savedTodoItems,action.payload]};
-        default: return state;
-    }
-}
+// export default function todoReducer(state=initialState, action){
+//     switch(action.type){
+//         case actions.UPDATE_TODO_LIST:
+//             return {savedTodoItems: [...initialState.savedTodoItems,action.payload]};
+//         default: return state;
+//     }
+// }
        
        
        
